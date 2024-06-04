a14b9615c3f0f2bef91055ec18fe451f87ef8a69
aborzym <borzym.a@gmail.com>
2024-06-04 18:57:32 +0200

    01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
+import Profile from "./Profile";
+import user from "../data/user.json";
+
 function App() {
   return (
     <>
-      <h1>React Homework Template (Vite)</h1>
+      <h1>goit-react-hw-01-components</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
   );
 }
